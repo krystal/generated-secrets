9bd576666a94aa617bd803366cb87edcd047cf62
Adam Cooke <adam@adam.ac>
2023-04-07 09:39:55 +0100

    fix: allow webhooks to be disabled

--- cmd/main.go
+++ cmd/main.go
@@ -96,10 +96,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "GeneratedSecret")
 		os.Exit(1)
 	}
-	if err = (&secretsv1.GeneratedSecret{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "GeneratedSecret")
-		os.Exit(1)
+
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&secretsv1.GeneratedSecret{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "GeneratedSecret")
+			os.Exit(1)
+		}
 	}
+
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
